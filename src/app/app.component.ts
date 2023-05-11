f61414ad7363e931ab9d430205406c6b3ea82ef1
TheGlitcher <forozcomercado@gmail.com>
2023-05-10 22:39:12 -0500

    ref: using template and styles component properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [
+    `
+      h1 {
+        color: tomato;
+      }
+    `
+  ]
 })
+
 export class AppComponent {
 }
