0b80ebbd9cb259b3aefc0fc61bf4ae6d19361c07
TheGlitcher <forozcomercado@gmail.com>
2023-05-13 10:54:15 -0500

    feat: using *ngIf structural directive

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -16,13 +16,14 @@ export class ServersComponent {
   public allowNewServer = false;
   public serverCreationStatus = 'No server was created';
   public serverName = '';
-
+  public serverWasCreated = false;
   constructor() {
     setTimeout(()=> this.allowNewServer = true,2000)
   }
 
   onCreateServer(){
-    this.serverCreationStatus = "Server was created! Server name:" + this.serverName;
+    this.serverCreationStatus = "Server was created! Server name: " + this.serverName;
+    this.serverWasCreated = true;
   }
 
   onUpdateServerName(event: Event) {
