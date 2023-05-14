32c95f4539f935222812d48301b91388d9cad8fb
TheGlitcher <forozcomercado@gmail.com>
2023-05-14 18:49:29 -0500

    feat: added the ingredients list to the shopping list component
    created the Ingredient model

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Ingredient} from "../shared/models/ingredient";
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
-
+  public ingredients: Ingredient[] = [
+    new Ingredient('Fish', 1),
+    new Ingredient('Tomatos', 3)
+  ]
 }
