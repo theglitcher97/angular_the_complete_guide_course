45b7a999f263daf406a72fa5e810c27b3be6e7c1
Fernando Orozco <forozcomercado@gmail.com>
2023-05-18 13:04:28 -0500

    feat: understanding angular lifecycle hooks

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, DoCheck, Input, OnChanges, OnInit} from '@angular/core';
 import {Server} from "../shared/types/server";
 
 @Component({
@@ -6,6 +6,25 @@ import {Server} from "../shared/types/server";
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck{
   @Input("serverElement") element!: Server;
+
+  constructor() {
+    console.log('on the constructor');
+  }
+
+  // Hook execute after any bind @input() property value has changed,
+  // it gets trigger before the OnInit
+  ngOnChanges($event: any){
+    console.log('OnChanges hook', $event)
+  }
+
+  // This hook gets trigger after the constructor
+  ngOnInit() {
+    console.log('on the OnInit')
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck invoked')
+  }
 }
