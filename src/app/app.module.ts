221918a001559addd5178bca928e89bbcfba40f9
Fernando Orozco <forozcomercado@gmail.com>
2023-05-19 23:57:28 -0500

    ref: set main as a start point for future projects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {ServerComponent} from "./server/server.component";
-import { ServersComponent } from './servers/servers.component';
-import {FormsModule} from "@angular/forms";
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
