56fe22e771f136784b67159061a10a7505b05cf4
Fernando Orozco <forozcomercado@gmail.com>
2023-05-23 23:02:47 -0500

    feat: store new ingredients

--- src/app/shopping-list/edit-shopping-list/edit-shopping-list.component.ts
+++ src/app/shopping-list/edit-shopping-list/edit-shopping-list.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from '../../shared/models/ingredient';
 
 @Component({
   selector: 'app-edit-shopping-list',
   templateUrl: './edit-shopping-list.component.html',
-  styleUrls: ['./edit-shopping-list.component.css']
+  styleUrls: ['./edit-shopping-list.component.css'],
 })
 export class EditShoppingListComponent {
+  @ViewChild('nameInput') nameInput!: ElementRef;
+  @ViewChild('amountInput') amountInput!: ElementRef;
 
+  @Output() onNewIngredients: EventEmitter<Ingredient> =
+    new EventEmitter<Ingredient>();
+
+  onAddIngredients() {
+    let name = this.nameInput.nativeElement.value;
+    let amount = this.amountInput.nativeElement.value;
+
+    if (name.trim().length === 0) return;
+    if (amount <= 0) return;
+    this.onNewIngredients.emit(new Ingredient(name, amount));
+
+    // reset the form
+    this.nameInput.nativeElement.value = '';
+    this.amountInput.nativeElement.value = '';
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
-import {Ingredient} from "../shared/models/ingredient";
+import { Ingredient } from '../shared/models/ingredient';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent {
   public ingredients: Ingredient[] = [
     new Ingredient('Fish', 1),
-    new Ingredient('Tomatos', 3)
-  ]
+    new Ingredient('Tomatos', 3),
+  ];
+
+  onAddNewIngredients(newIngredients: Ingredient) {
+    this.ingredients.push(newIngredients);
+  }
 }
