e102befbdce21d7a774c34c0652b4f99389933e5
Fernando Orozco <forozcomercado@gmail.com>
2023-05-25 18:55:40 -0500

    feat: binding to properties in our directive
    Angular first search for properties in the directives and then the component

--- src/app/directives/advance-high-light.directive.ts
+++ src/app/directives/advance-high-light.directive.ts
@@ -3,6 +3,7 @@ import {
   ElementRef,
   HostBinding,
   HostListener,
+  Input,
   OnInit,
   Renderer2,
 } from '@angular/core';
@@ -11,14 +12,30 @@ import {
   selector: '[appAdvanceHighlight]',
 })
 export class AdvanceHighlightDirective implements OnInit {
+  @Input() defaultColor = 'transparent';
+  @Input() highlightColor = 'yellow';
   // with this set up, we have direct access to this property on this element
   @HostBinding('style.backgroundColor') backgroundColor!: string;
+  private hasRaisedClass = false;
 
   constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
 
   // This decorator allow ud to listen for a specified event that we want to react to
   @HostListener('mouseenter') mouseenter(eventData: Event) {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'font-size', '24px');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'font-size', '24px');
+    this.backgroundColor = this.highlightColor;
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'font-size', '16px');
+    this.backgroundColor = this.defaultColor;
+  }
+
+  @HostListener('click') onClick(eventDat: Event) {
+    if (this.hasRaisedClass)
+      this.renderer.removeClass(this.elementRef.nativeElement, 'raised');
+    else this.renderer.addClass(this.elementRef.nativeElement, 'raised');
+    this.hasRaisedClass = !this.hasRaisedClass;
   }
 
   /*@HostListener('mouseover') mouseover() {
@@ -39,11 +56,6 @@ export class AdvanceHighlightDirective implements OnInit {
 
   // This decorator allow us to bind to a property of the element this directive sits on
 
-  @HostListener('mouseleave') mouseleave(eventData: Event) {
-    console.log(eventData);
-    this.renderer.setStyle(this.elementRef.nativeElement, 'font-size', '16px');
-  }
-
   ngOnInit() {
     /*this.renderer.setStyle(
       this.elementRef.nativeElement,
@@ -53,6 +65,6 @@ export class AdvanceHighlightDirective implements OnInit {
 
     // with this property, and because we set it up for it, we can modify
     // the backgroundColor of the element directly
-    this.backgroundColor = 'green';
+    this.backgroundColor = this.defaultColor;
   }
 }
