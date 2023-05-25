e16d4fce1f88db4ac28b718c00cfa3096349f960
Fernando Orozco <forozcomercado@gmail.com>
2023-05-25 18:46:39 -0500

    feat: binging to mouse event and element properties using @hostListener() and @HostBinding() respectively

--- src/app/directives/advance-high-light.directive.ts
+++ src/app/directives/advance-high-light.directive.ts
@@ -1,6 +1,7 @@
 import {
   Directive,
   ElementRef,
+  HostBinding,
   HostListener,
   OnInit,
   Renderer2,
@@ -10,22 +11,48 @@ import {
   selector: '[appAdvanceHighlight]',
 })
 export class AdvanceHighlightDirective implements OnInit {
+  // with this set up, we have direct access to this property on this element
+  @HostBinding('style.backgroundColor') backgroundColor!: string;
+
   constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
 
-  // This decorator is listening for a specified event that we can react to
+  // This decorator allow ud to listen for a specified event that we want to react to
   @HostListener('mouseenter') mouseenter(eventData: Event) {
     this.renderer.setStyle(this.elementRef.nativeElement, 'font-size', '24px');
   }
 
+  /*@HostListener('mouseover') mouseover() {
+    this.renderer.setStyle(
+      this.elementRef.nativeElement,
+      'background-color',
+      'purple'
+    );
+  }*/
+
+  /*@HostListener('mouseout') mouseout() {
+    this.renderer.setStyle(
+      this.elementRef.nativeElement,
+      'background-color',
+      'transparent'
+    );
+  }*/
+
+  // This decorator allow us to bind to a property of the element this directive sits on
+
   @HostListener('mouseleave') mouseleave(eventData: Event) {
+    console.log(eventData);
     this.renderer.setStyle(this.elementRef.nativeElement, 'font-size', '16px');
   }
 
   ngOnInit() {
-    this.renderer.setStyle(
+    /*this.renderer.setStyle(
       this.elementRef.nativeElement,
       'background-color',
       'green'
-    );
+    );*/
+
+    // with this property, and because we set it up for it, we can modify
+    // the backgroundColor of the element directly
+    this.backgroundColor = 'green';
   }
 }
