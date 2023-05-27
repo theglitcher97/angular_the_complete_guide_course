822ffee186b0ab774279fed10f9e011c0eefacfa
Fernando Orozco <forozcomercado@gmail.com>
2023-05-27 13:17:21 -0500

    ref: send event with the new habit added info
    display an alert, informing the user

--- src/app/add-habit/add-habit.component.ts
+++ src/app/add-habit/add-habit.component.ts
@@ -1,5 +1,11 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { ImpactEnum } from '../dashboard/models/habit';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Habit, ImpactEnum } from '../dashboard/models/habit';
 
 @Component({
   selector: 'app-add-habit',
@@ -11,23 +17,44 @@ export class AddHabitComponent {
   @ViewChild('habitImpact') habitImpact!: ElementRef;
   @ViewChild('habitTimesADay') habitTimesADay!: ElementRef;
   @ViewChild('habitDaysPerWeek') habitDaysPerWeek!: ElementRef;
+  @Output() newHabitAddedEvent: EventEmitter<Habit> = new EventEmitter<Habit>();
+  public newHabitAddedMessage = 'New habit was added successfully!';
+  public displayMessage = false;
   protected readonly ImpactEnum = ImpactEnum;
 
   onSubmit() {
-    console.log('name -> ', this.habitName.nativeElement.value);
-    console.log('impact -> ', this.habitImpact.nativeElement.value);
-    console.log('tad -> ', this.habitTimesADay.nativeElement.value);
-    console.log('dpw -> ', this.habitDaysPerWeek.nativeElement.value);
-    console.log('Productive level -> ', this.calculateProductivity());
+    const name = this.habitName.nativeElement.value as string;
+    const impact = this.habitImpact.nativeElement.value;
+    const times_a_day = this.habitTimesADay.nativeElement.value as number;
+    const days_per_week = this.habitDaysPerWeek.nativeElement.value as number;
+
+    if (!this.isHabitValid(name, impact, times_a_day, days_per_week)) return;
+
+    this.newHabitAddedEvent.emit(
+      new Habit(name, impact, times_a_day, days_per_week)
+    );
+
+    this.displayMessage = true;
+    setTimeout(() => (this.displayMessage = false), 2000);
+
+    this.resetForm();
   }
 
-  /**
-   * @Deprecated this method is just for testing
-   * @private
-   */
-  private calculateProductivity() {
-    return ImpactEnum.positive === this.habitImpact.nativeElement.value
-      ? 100
-      : 0;
+  private isHabitValid(
+    name: string,
+    impact: string,
+    times_a_day: number,
+    days_per_week: number
+  ) {
+    return (
+      name.trim().length > 0 && impact && times_a_day > 0 && days_per_week > 0
+    );
+  }
+
+  private resetForm() {
+    this.habitName.nativeElement.value = '';
+    this.habitImpact.nativeElement.value = this.ImpactEnum.positive;
+    this.habitTimesADay.nativeElement.value = 1;
+    this.habitDaysPerWeek.nativeElement.value = 1;
   }
 }
