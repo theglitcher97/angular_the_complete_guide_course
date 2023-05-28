35b64750363a484aee771fd4ac06feed17108bef
Fernando Orozco <forozcomercado@gmail.com>
2023-05-27 20:26:22 -0500

    feat: search bar functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Habit, ImpactEnum } from './dashboard/models/habit';
 
 @Component({
@@ -6,14 +6,19 @@ import { Habit, ImpactEnum } from './dashboard/models/habit';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public selectedTab = 'dashboard';
   public habit!: Habit | undefined;
+  public habitsCopy!: Habit[];
   public habits: Habit[] = [
     new Habit('Exercise', ImpactEnum.positive, 4, 4),
     new Habit('Drink Alcohol', ImpactEnum.negative, 2, 4),
     new Habit('Go to work', ImpactEnum.neutral, 1, 5),
   ];
+
+  ngOnInit() {
+    this.habitsCopy = [...this.habits];
+  }
 
   onNavigate(tab: string) {
     this.selectedTab = tab;
@@ -22,11 +27,15 @@ export class AppComponent {
 
   onNewHabitCreated(habit: Habit) {
     this.habits.push(habit);
+    this.habitsCopy = [...this.habits];
   }
 
   onRemoveHabit(habit: Habit) {
     let index = this.habits.findIndex((h) => habit.id === h.id);
-    if (index !== -1) this.habits.splice(index, 1);
+    if (index !== -1) {
+      this.habits.splice(index, 1);
+      this.habitsCopy = [...this.habits];
+    }
   }
 
   onEditHabit(habit: Habit) {
@@ -36,6 +45,16 @@ export class AppComponent {
 
   onHabitEdited(habit1: Habit) {
     const index = this.habits.findIndex((habit) => habit.id === habit1.id);
-    if (index !== -1) this.habits[index] = habit1;
+    if (index !== -1) {
+      this.habits[index] = habit1;
+      this.habitsCopy = [...this.habits];
+    }
+  }
+
+  onSearchHabit(name: string) {
+    this.habits = [...this.habitsCopy];
+    this.habits = this.habits.filter((habit) =>
+      habit.name.toLowerCase().includes(name)
+    );
   }
 }
