e91de9ddd023c7e776f23d76991cf2194153d4f2
Fernando Orozco <forozcomercado@gmail.com>
2023-06-01 21:20:40 -0500

    feat: reading values from params and queryParams

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,6 +12,9 @@ export class HomeComponent implements OnInit {
   ngOnInit() {}
 
   onLoadServers() {
-    this.router.navigate(['/servers']);
+    this.router.navigate(['/servers', 1, 'edit'], {
+      queryParams: { allowEdit: 1 },
+      fragment: 'loading',
+    });
   }
 }
--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -12,9 +13,14 @@ export class EditServerComponent implements OnInit {
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) {}
+  constructor(
+    private serversService: ServersService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    console.log(this.activatedRoute.snapshot.params);
+    console.log(this.activatedRoute.snapshot.queryParams);
     this.server = this.serversService.getServer(1);
     if (this.server) {
       this.serverName = this.server.name;
