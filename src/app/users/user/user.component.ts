49d33a10190c9906ea9448e1b97a22e0659080e0
Fernando Orozco <forozcomercado@gmail.com>
2023-06-01 20:41:20 -0500

    feat: unsubscribing from manual subscriptions on onDestroy

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css'],
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user!: { id: number; name: string };
   protected readonly Math = Math;
+  private subscriptions: Subscription = new Subscription();
 
   constructor(private activeRoute: ActivatedRoute) {}
 
@@ -17,10 +19,15 @@ export class UserComponent implements OnInit {
       id: this.activeRoute.snapshot.params['id'],
       name: this.activeRoute.snapshot.params['name'],
     };
+    this.subscriptions.add(
+      this.activeRoute.params.subscribe((param: Params) => {
+        this.user.id = param['id'];
+        this.user.name = param['name'];
+      })
+    );
+  }
 
-    this.activeRoute.params.subscribe((param: Params) => {
-      this.user.id = param['id'];
-      this.user.name = param['name'];
-    });
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
   }
 }
