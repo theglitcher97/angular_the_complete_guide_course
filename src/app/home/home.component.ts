3b233c341c6d1a84cd3fcc684c19011a46fd25cf
Fernando Orozco <forozcomercado@gmail.com>
2023-06-11 11:14:05 -0500

    ref: creating our own interval observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { Observable, Observer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -20,12 +20,32 @@ export class HomeComponent implements OnInit, OnDestroy {
     //! even if you leave (destroy) this component, and it gets worse, everytime
     //! this component is created, it creates a new Observable.
     //? this observable never completes by itself
-    const intervalSubscription = interval(1000).subscribe((count) => {
+    /*const intervalSubscription = interval(1000).subscribe((count) => {
       this.counter = count;
       console.log(count);
     });
+    this.subscriptions.add(intervalSubscription);*/
 
-    this.subscriptions.add(intervalSubscription);
+    // Creating out custom intervalObservable
+    const customIntervalObservable = Observable.create(
+      (observer: Observer<number>) => {
+        //? "observer" is a parameter pass by rxjs to our function
+        let counter = 0;
+        setInterval(() => {
+          //? the "next" method is use to send data to anyone subscribed to this observable
+          counter++;
+          observer.next(counter);
+        }, 1000);
+      }
+    );
+
+    const customIntervalSubscription = customIntervalObservable.subscribe(
+      (count: number) => {
+        this.counter = count;
+        console.log(count);
+      }
+    );
+    this.subscriptions.add(customIntervalSubscription);
   }
 
   ngOnDestroy() {
