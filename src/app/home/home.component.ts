8cea2c49b2a8bca4dccb2cded815a0b5ad59c47d
Fernando Orozco <forozcomercado@gmail.com>
2023-06-11 11:02:16 -0500

    ref: handle observables creation and unsubscription

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  public counter: any;
 
-  constructor() { }
+  private subscriptions: Subscription = new Subscription();
+
+  constructor() {}
 
   ngOnInit() {
+    // emitting event with Interval rxjs operator
+    //! BE CAREFUL, Angular won't unsubscribe from this observable,
+    //! if you don't unsubscribe from this observable, it will live FOREVER!!
+    //! even if you leave (destroy) this component, and it gets worse, everytime
+    //! this component is created, it creates a new Observable.
+    //? this observable never completes by itself
+    const intervalSubscription = interval(1000).subscribe((count) => {
+      this.counter = count;
+      console.log(count);
+    });
+
+    this.subscriptions.add(intervalSubscription);
   }
 
+  ngOnDestroy() {
+    //? this will unsubscribe the intervalSubscription just before the component is destroy
+    //? and any other subscription we have store in "subscriptions"
+    //? this prevent us from creating memory leaks in our program
+    this.subscriptions.unsubscribe();
+  }
 }
