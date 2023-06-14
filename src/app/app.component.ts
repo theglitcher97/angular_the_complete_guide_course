0bc498543a93eb6c8c72011ea0b1b44dcb3e7e77
Fernando Orozco <forozcomercado@gmail.com>
2023-06-13 23:10:50 -0500

    ref: use of subjects to manage the application state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  constructor() {}
+  public userActivated = false;
+  private subscriptions = new Subscription();
 
-  ngOnInit() {}
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.subscriptions.add(
+      this.userService.activatedEmitter.subscribe((isActivated) => {
+        this.userActivated = isActivated;
+      })
+    );
+  }
 }
